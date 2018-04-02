c046d016315252b9b190f349a1ddc614988c24dd
bianguangkuo <bgk083@126.com>
2018-04-02 15:55:29 +0800

    first commit and add test

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # image-cropper
 Given an image, image-cropper splits the image into several pieces
+
+## How to run
+
+```python
+cd image-cropper
+cropper.py "F:\limitless-github\image-cropper\test\test.jpg" "F:\limitless-github\image-cropper\test\test" 2 4
+```
---
+++ image-cropper/cropper.py
@@ -0,0 +1,33 @@
+import sys
+from PIL import Image
+
+
+class ImageCropper:
+    def crop_image(self, img_input_path, img_output_path, x=1, y=2, img_suffix='jpg'):
+        img = Image.open(img_input_path)
+        w, h = img.size
+        width_piece = w / x
+        height_piece = h / y
+        for i in range(y):
+            for j in range(x):
+                print((width_piece * j, height_piece * i, width_piece * (j + 1), height_piece * (i + 1)))
+                img.crop((width_piece * j, height_piece * i, width_piece * (j + 1), height_piece * (i + 1))).save('{}{}{}.{}'.format(img_output_path, i, j, img_suffix))
+
+
+if __name__ == "__main__":
+    img_input_path = sys.argv[1]
+    img_output_path = sys.argv[2]
+    print(img_input_path)
+    print(img_output_path)
+    image_cropper = ImageCropper()
+    if len(sys.argv) >= 5:
+        img_x_pieces = int(sys.argv[3])
+        img_y_pieces = int(sys.argv[4])
+        if len(sys.argv) >= 6 :
+            img_suffix = sys.argv[5]
+            print(img_x_pieces, img_y_pieces, img_suffix)
+            image_cropper.crop_image(img_input_path, img_output_path, img_x_pieces, img_y_pieces, img_suffix)
+        else:
+            image_cropper.crop_image(img_input_path, img_output_path, img_x_pieces, img_y_pieces)
+    else:
+        image_cropper.crop_image(img_input_path, img_output_path)

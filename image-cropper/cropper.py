0d755e907747a40f289858f52bf165b0ac48d824
bianguangkuo <bgk083@126.com>
2018-04-04 15:10:08 +0800

    add crop_image_in_dir function

--- image-cropper/cropper.py
+++ image-cropper/cropper.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PIL import Image
 
@@ -12,6 +13,19 @@ class ImageCropper:
             for j in range(x):
                 img.crop((width_piece * j, height_piece * i, width_piece * (j + 1), height_piece * (i + 1))).save('{}{}{}.{}'.format(img_output_path, i, j, img_suffix))
 
+    def crop_image_in_dir(self, img_input_path, img_output_path, x=1,y=2):
+        for path, dir_list, file_list in os.walk(img_input_path):
+            for file in file_list:
+                print(os.path.join(path, file))
+                [file_prefix, file_suffix] = str.split(file, '.')
+                self.crop_image(
+                    os.path.join(path, file),
+                    os.path.join(img_output_path, file_prefix),
+                    x,
+                    y,
+                    file_suffix
+                )
+
 
 if __name__ == "__main__":
     img_input_path = sys.argv[1]
@@ -21,10 +35,6 @@ if __name__ == "__main__":
     if len(sys.argv) >= 5:
         img_x_pieces = int(sys.argv[3])
         img_y_pieces = int(sys.argv[4])
-        if len(sys.argv) >= 6 :
-            img_suffix = sys.argv[5]
-            image_cropper.crop_image(img_input_path, img_output_path, img_x_pieces, img_y_pieces, img_suffix)
-        else:
-            image_cropper.crop_image(img_input_path, img_output_path, img_x_pieces, img_y_pieces)
+        image_cropper.crop_image_in_dir(img_input_path, img_output_path, img_x_pieces, img_y_pieces)
     else:
-        image_cropper.crop_image(img_input_path, img_output_path)
+        image_cropper.crop_image_in_dir(img_input_path, img_output_path)
